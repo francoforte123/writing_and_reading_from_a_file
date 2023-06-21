9feeccbbefe32852026b7298b08ca63d042136aa
Emmanuele La Duca <laducaemanuele20@gmail.com>
2023-06-21 17:17:50 +0200

    Aggiungere i file di progetto.

---
+++ writing_and_reading_from_a_file/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("insert the path for reading the file= ");
+String pathFile= Console.ReadLine();
+
+String contentFile= File.ReadAllText(pathFile);
+
+Console.WriteLine("\nthe content the file is:");
+Console.WriteLine(contentFile);
+
+/*
+    è possibile leggere il contenuto del file di testo (per leggere il contenuto del testo, va messo tutto il percorso del file, e non 
+    questa espressione ./nomeDelFile.txt)
+ */
+//non riesco ad inserire un file PowerPoint
+
+
+
+
+
